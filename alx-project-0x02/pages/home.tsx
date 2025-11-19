7c9e27dd2010e2eef617280195aad4a17342d602
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-11-19 03:49:19 +0100

    fix

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,11 +1,15 @@
+import Card from '@/components/common/Card';
 import React from 'react';
 
-const Homepage: React.FC = () => {
+const Home: React.FC = () => {
   return (
     <div>
-        <h1>Welcome to Home</h1>
+      <h1>Homepage</h1>
+      <Card  title='cardname1' content='card description1'/>
+      <Card  title='cardname2' content='card description2'/>
+      <Card  title='cardname3' content='card description3'/>
     </div>
   );
 };
 
-export default Homepage;
+export default Home;
--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,13 +1,9 @@
-import Card from '@/components/common/Card';
 import React from 'react';
 
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>Homepage</h1>
-      <Card  title='cardname1' content='card description1'/>
-      <Card  title='cardname2' content='card description2'/>
-      <Card  title='cardname3' content='card description3'/>
+        <h1>Welcome to Home</h1>
     </div>
   );
 };
